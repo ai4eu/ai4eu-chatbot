f7a750a13beedf5161844106f915934900c81eaf
Panagiotis Papadakos <papadako@ics.forth.gr>
2021-11-24 11:03:40 +0200

    [GoBot] Show the predicted action in the logs

--- src/go_bot/ai4eu_go_bot.py
+++ src/go_bot/ai4eu_go_bot.py
@@ -190,7 +190,7 @@ class AI4EUGoalOrientedBot(NNModel):
         self.QA = ChatBot_QA()
         # The following could be parameters in the json configuration
         self.topk = 3   # Topk results for qa module
-        self.action_probability_threshold = 1.0  # Threshold for action probabilities - Have to fine tune this
+        self.action_probability_threshold = 0.1  # Threshold for action probabilities - Have to fine tune this
 
         self.reset()
 
@@ -423,16 +423,20 @@ class AI4EUGoalOrientedBot(NNModel):
 
         # Policy Making -> Predict new action
 
-        # predict the action to perform (e.g. response smth or call the api)
+        # predict the action to perform (e.g. response something or call any of the QA or search APIs)
         utterance_batch_features, policy_prediction = self._infer(user_text, user_tracker)
+
+        # Print the predicted action
+        print(f"Predicted action is = '{self.nlg_manager.get_action(policy_prediction.predicted_action_ix)}'")
 
         # AI4EU We have to check the probability of the actions.
         # If they are too low then it is better to use the QA module
         print(f"Probability of predicted action = '{policy_prediction.probs[policy_prediction.predicted_action_ix]}'")
         if policy_prediction.probs[policy_prediction.predicted_action_ix] < self.action_probability_threshold:
             policy_prediction.predicted_action_ix = self.nlg_manager.get_ai4eu_qa_api_call_action_id()
-            print(f"Predicted action is = '{ self.nlg_manager.get_action(policy_prediction.predicted_action_ix)}'")
             print(f"Fall-back to QA since prob is = '{ policy_prediction.probs[policy_prediction.predicted_action_ix]}'")
+
+        print(f"Use action = '{ self.nlg_manager.get_action(policy_prediction.predicted_action_ix)}'")
 
         user_tracker.update_previous_action(policy_prediction.predicted_action_ix)
         user_tracker.network_state = policy_prediction.get_network_state()
